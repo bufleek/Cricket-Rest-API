7bbc918dbb27699760e9b58a37594088dcb68c2d
Cephas Too <cephaske254@gmail.com>
2022-04-27 10:31:08 +0300

    integrate scrapper with production backend

--- scrapper/src/api/index.ts
+++ scrapper/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: process.env.NODE_ENV==="production"? "http://64.225.1.125/api":"http://localhost:8000/api",
   headers: {
     "Content-type": "application/json",
   },
